b945e029963241879f92199e4552cade3c50bf1f
MamoudouDiallo <diallomoud99@gmail.com>
2023-02-21 00:20:58 +0000

    prémier commit

---
+++ src/main/java/com/mood/bibliothequemood/BibliothequemoodApplication.java
@@ -0,0 +1,13 @@
+package com.mood.bibliothequemood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequemoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequemoodApplication.class, args);
+	}
+
+}
