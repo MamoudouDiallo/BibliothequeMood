154c3718123c7938b0cc2b48a3e65629b885d875
amadou98 <amadoubhoyediallo98@gmail.com>
2023-03-04 22:33:45 +0000

    rename classes

--- src/main/java/com/mood/bibliothequemood/handlers/ErrorHandingControllerAdvice.java
+++ src/main/java/com/mood/bibliothequemood/handlers/ErrorHandingControllerAdvice.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ApplicationExceptionHandler {
+public class ErrorHandingControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
